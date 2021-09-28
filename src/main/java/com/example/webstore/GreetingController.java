e9ae0fd99042f846adb073c1ded075fc357a6fe7
Niporka <pavelniporka@gmail.com>
2021-09-28 16:57:46 +0300

    Start

--- src/main/java/com/example/webstore/GreetingController.java
+++ src/main/java/com/example/webstore/GreetingController.java
@@ -2,16 +2,12 @@ package com.example.webstore;
 
 import com.example.webstore.domain.Message;
 import com.example.webstore.repos.MessageRepo;
-import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -19,16 +15,12 @@ public class GreetingController {
     @Autowired
     private MessageRepo messageRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
 
         Iterable<Message> messages = messageRepo.findAll();
@@ -38,7 +30,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
 
         Message message = new Message(text, tag);
--- src/main/java/com/example/webstore/repos/MessageRepo.java
+++ src/main/java/com/example/webstore/repos/MessageRepo.java
@@ -1,6 +1,7 @@
 package com.example.webstore.repos;
 
 import com.example.webstore.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
