9d8061cd0865ec7047690e7cfc32a3fd9d0eaacb
Niporka <pavelniporka@gmail.com>
2021-09-28 15:44:31 +0300

    Start

---
+++ src/main/java/com/example/webstore/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.webstore.repos;
+
+import com.example.webstore.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
